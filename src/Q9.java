9cb7c04893bcf0d75455eb926eef9745d6d777b8
Bianca Valicec <145712847+GlitchWhale@users.noreply.github.com>
2023-12-14 13:30:18 +0000

    Delete src directory

--- src/Q9.java
+++
@@ -1,5 +0,0 @@
-public class Q9 {
-    //  Question 9 – Backtracking through a Maze (Stack)
-    //  Implement a backtracking algorithm, using a Stack, to find a path through a maze from start to exit.
-    //  Refer to description of algorithm (from textbook) in the accompanying PDF.
-}
